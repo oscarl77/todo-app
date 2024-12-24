4865c3e7031d38d626e5389f09d7e79a5f953546
oscarl77 <oscarlaurens18@gmail.com>
2024-12-24 02:16:32 +0100

    Disabled contextLoads() test for now.

--- todo-backend/src/test/java/com/oscarl/todobackend/TodoBackendApplicationTests.java
+++ todo-backend/src/test/java/com/oscarl/todobackend/TodoBackendApplicationTests.java
@@ -1,5 +1,6 @@
 package com.oscarl.todobackend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TodoBackendApplicationTests {
 
     @Test
+    @Disabled("Not testing context loading right now")
     void contextLoads() {
     }
 
