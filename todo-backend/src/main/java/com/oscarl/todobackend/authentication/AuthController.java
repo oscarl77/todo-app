51624e7aa333043ba2912f6b6d866c3bea5aa772
oscarl77 <oscarlaurens18@gmail.com>
2025-04-18 22:00:18 +0100

    Took generateToken() out of the controller into the service.

--- todo-backend/src/main/java/com/oscarl/todobackend/authentication/AuthController.java
+++ todo-backend/src/main/java/com/oscarl/todobackend/authentication/AuthController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller class that handles login validation.
+ */
 @RestController
 @RequestMapping("api/auth")
 public class AuthController {
@@ -28,33 +31,14 @@ public class AuthController {
         String password = loginRequest.getPassword();
 
         if (authService.isLoginValid(email, password)) {
-            // Generate JWT token and store as HTTP-only cookie
-            String token = authService.generateToken(email);
-            ResponseCookie jwtCookie = createJwtCookie(token);
-
+            // Generate JWT cookie and store as HTTP-only cookie
+            ResponseCookie jwtCookie = authService.createJwtCookie(email);
+            // Return JWT cookie to frontend
             return ResponseEntity.ok()
                     .header(HttpHeaders.SET_COOKIE, jwtCookie.toString())
                     .body("Login successful");
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
     }
-
-    /**
-     * Creates an HTTP-only cookie to store JWT token.
-     * @param token the JWT token.
-     * @return the HTTP-only cookie.
-     */
-    private ResponseCookie createJwtCookie(String token) {
-        int SECONDS = 60;
-        int MINUTES = 60;
-        return ResponseCookie.from("jwt", token)
-                .httpOnly(true) // Make cookie inaccessible via JavaScript
-                .secure(true)
-                .path("/") // Cookie is valid for all endpoints
-                .maxAge(SECONDS * MINUTES)
-                .sameSite("Strict") // CSRF protection
-                .build();
-    }
-
 
 }
